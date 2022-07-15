fffbb9b06299543b27815fd016f5a96c1cb48985
Marharyta Bosak <aboimova.rita@gmail.com>
2022-07-15 14:33:17 +0300

    Revert "code refactoring"
    This reverts commit 2fae4ce84c67ec808fb6f1bdf4b1c026aa0cf687.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,93 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import { API_KEY } from '../service/apiRequest';
 import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Button from './Button/Button';
+import Loader from './Loader/Loader';
+import Modal from './Modal/Modal';
+import Message from './Message/Message';
 import { AppStyle } from './App.styled';
-import GalleryView from './GalleryView/GalleryView';
 
 const App = () => {
   const [imageName, setImageName] = useState('');
+  const [images, setImages] = useState(null);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [modalImage, setModalImage] = useState(null);
+  const [showBtn, setShowBtn] = useState(false);
+  const [perPage] = useState(12);
+
+  useEffect(() => {
+    if (imageName === '') {
+      return;
+    }
+
+    setIsLoading(true);
+    setPage(1);
+    setShowBtn(false);
+
+    const fetchData = async () => {
+      const response = await axios.get(
+        `/?q=${imageName}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`
+      );
+
+      const responseData = response.data.hits;
+
+      if (responseData.length >= perPage) {
+        setShowBtn(true);
+      }
+
+      setImages(responseData);
+      setPage(state => (state += 1));
+    };
+
+    fetchData();
+
+    setIsLoading(false);
+  }, [imageName, perPage]);
+
+  const handleLoadMoreBtn = async () => {
+    const response = await axios.get(
+      `/?q=${imageName}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`
+    );
+
+    const responseData = response.data.hits;
+
+    if (responseData.length < perPage) {
+      setShowBtn(false);
+    }
+
+    setImages(state => [...state, ...responseData]);
+    setPage(state => (state += 1));
+  };
+
+  const handleClickImage = imageId => {
+    const modalImage = images.find(image => image.id === imageId);
+
+    setModalImage(modalImage);
+  };
+
+  const handleModalClose = () => {
+    setModalImage(null);
+  };
 
   return (
     <AppStyle>
       <Searchbar onSubmit={setImageName} />
-      <GalleryView imageName={imageName} />
+      {images && images.length === 0 && (
+        <Message
+          message="We can`t find pictures by this name. Please check your search
+            request"
+        />
+      )}
+      {images && <ImageGallery images={images} onClick={handleClickImage} />}
+      {showBtn && <Button onClick={handleLoadMoreBtn} />}
+      {isLoading && <Loader />}
+      {modalImage && (
+        <Modal modalImage={modalImage} onClose={handleModalClose} />
+      )}
     </AppStyle>
   );
 };
